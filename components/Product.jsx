7d756dfd5521b1de6b5bf3b2c78ce085eaf4b362
Jaison <jaisonschmidt@gmail.com>
2024-06-05 20:47:08 -0300

    finaliza componente product

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,7 +1,24 @@
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
+import { Button, Card } from '@rneui/themed';
 
 export function Product({item}) {
     return (
-        <Text>{item.title}</Text>
+        <Card>
+            <Card.Title>{item.title}</Card.Title>
+            <Card.Divider />
+            <View style={{flexDirection: 'row'}}>
+                <Image 
+                    source={{uri: item.image}} 
+                    width={80} 
+                    height={80} 
+                />
+                <View style={{paddingLeft: 20}}>
+                    <Text style={{fontSize: 20}}>{item.price}</Text>
+                    <Button>
+                        Add cart
+                    </Button>
+                </View>
+            </View>
+        </Card>
     );
 }
